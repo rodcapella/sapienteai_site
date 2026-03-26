0319b2349398dd7cdec7681b73bf6d5d80d45c99
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-26 17:43:52 +0000

    fix import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Switch } from "wouter";
 import Home from "./pages/Home";
-import { useTranslation } from '@/hooks/useTranslation';
+import { useTranslation } from '../hooks/useTranslation';
 
 const { t } = useTranslation();
 
