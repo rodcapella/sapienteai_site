a444d6fb4cf3083ea29b7e13bf38655c98dfbb0b
rodcapella <53801956+rodcapella@users.noreply.github.com>
2026-03-31 18:48:38 +0100

    Delete client/src/pages/content/pt directory

--- client/src/pages/content/pt/notFound.ts
+++
@@ -1,6 +0,0 @@
-export const notFoundPT = {
-  title: "Página não encontrada",
-  subtitle:
-    "Essa página não existe neste sistema.\nMas eu sei exatamente para onde te levar.",
-  button: "Voltar para Home"
-};
